a50732608554885ffe6552cb8badc063940eaea9
TalkBog <99033517+TalkBog@users.noreply.github.com>
2023-03-28 16:33:41 +0200

    ajout de test sur AccountTest.java

---
+++ src/main/java/ex_deux/Account.java
@@ -0,0 +1,40 @@
+package ex_deux;
+
+public class Account {
+    private int id;
+    private int solde;
+    private int taux;
+
+    public Account(int id, int solde, int taux) {
+        this.id = id;
+        this.solde = solde;
+        this.taux = taux;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public int getSolde() {
+        return solde;
+    }
+    public int getTaux() {
+        return taux;
+    }
+    public void setTaux(int taux) {
+        this.taux = taux;
+    }
+
+    public void retrait(int montant){
+        solde -= montant;
+    }
+    public void depot(int montant){
+        solde += montant;
+    }
+    public void transfert(int montant, Account account){
+        retrait(montant);
+        account.depot(montant);
+    }
+    public int interet(){
+        return solde * (taux/100);
+    }
+}
---
+++ src/main/java/ex_deux/Bank.java
@@ -0,0 +1,30 @@
+package ex_deux;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Bank {
+    private List<Account> accounts;
+
+    public Bank() {
+        accounts = new ArrayList<>();
+    }
+
+    public void addAccount(Account account){
+        accounts.add(account);
+    }
+
+    public void removeAccount(Account account){
+        accounts.remove(account);
+    }
+
+    public void afficherSoldes(){
+        for(Account account : accounts){
+            System.out.println(account.getId() + " : " + account.getSolde());
+        }
+    }
+
+    public void transfert(Account account , Account account1, int montant){
+        account.transfert(montant, account1);
+    }
+}
