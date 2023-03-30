e27a3a27c0837e81f454462c47f64aae3a4925ba
TalkBog <99033517+TalkBog@users.noreply.github.com>
2023-03-30 15:39:02 +0200

    commit

--- src/main/java/ex_deux/Bank.java
+++ src/main/java/ex_deux/Bank.java
@@ -2,13 +2,9 @@ package ex_deux;
 
 import java.util.ArrayList;
 import java.util.List;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 
 public class Bank {
-
-    private static final Logger logger = LogManager.getLogger(Bank.class);
     private List<Account> accounts;
 
     public Bank() {
@@ -28,7 +24,7 @@ public class Bank {
     public void afficherSoldes(){
         for(Account account : accounts){
             String str = account.getId() + " : " + account.getSolde();
-            logger.info(str);
+            System.out.println(str);
         }
     }
 
