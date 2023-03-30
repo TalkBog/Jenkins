1196ed3a2025929d2954e251e8a7a9cab228f943
TalkBog <99033517+TalkBog@users.noreply.github.com>
2023-03-30 16:12:57 +0200

    commit

--- src/main/java/ex_un/Book.java
+++ src/main/java/ex_un/Book.java
@@ -1,5 +1,39 @@
 package ex_un;
 
-public class Book {
+import java.util.Date;
 
+public class Book {
+    private String title;
+    private String author;
+    private Date date;
+
+    public Book(String title, String author, Date date) {
+        this.title = title;
+        this.author = author;
+        this.date = date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
 }
